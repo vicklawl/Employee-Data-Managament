3af20d7b0a3096b10c0b85b30e19f12a7b2535ec
Ravi Akkiraju <raviakkiraju@UM-L51FHTD7.local>
2019-01-19 13:02:09 -0500

    updated html. added jQuery link

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -21,7 +21,7 @@ function monthDiffFromToday(dateString) {
   var d2 = new Date();
   // var d2 = today.getMonth() + "/" + today.getDay() + "/" + today.getFullYear();
   months = (d2.getFullYear() - d1.getFullYear()) * 12;
-  months -= d1.getMonth() + 1;
+  months -= d1.getMonth();
   months += d2.getMonth();
   return months <= 0 ? 0 : months;
 }
